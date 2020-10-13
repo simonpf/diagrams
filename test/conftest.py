5c5f2a44f373bc05a0a585b534536fbcdee33b78
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2020-10-13 18:45:32 +0200

    Added mock classes for tkinter objects

--- test/conftest.py
+++ test/conftest.py
@@ -3,7 +3,42 @@ Global fixtures to allow testing tkinter apps.
 """
 import pytest
 
+class Tk:
+    """
+    Mock of the tkinter.Tk class.
+    """
+    def __init__(*args, **kwargs):
+        pass
+
+    def mainloop(*args, **kwargs):
+        pass
+
+class Canvas:
+    """
+    Mock of the tkinter.Canvas class.
+    """
+
+    def __init__(*args, **kwargs):
+        pass
+
+    def create_rectangle(*args, **kwargs):
+        pass
+
+    def create_text(*args, **kwargs):
+        pass
+
+    def create_line(*args, **kwargs):
+        pass
+
+    def create_polygon(*args, **kwargs):
+        pass
+
+    def pack(*args, **kwargs):
+        pass
+
+
 @pytest.fixture(autouse=True)
 def patch_mainloop(monkeypatch):
     do_nothing = lambda x: None
-    monkeypatch.setattr("tkinter.Tk.mainloop", do_nothing)
+    monkeypatch.setattr("tkinter.Tk", Tk)
+    monkeypatch.setattr("tkinter.Canvas", Canvas)
