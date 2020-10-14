3755335a94293f3e1f61884aff681efe1287e602
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2020-10-14 07:44:15 +0200

    Use wildcard imports.

--- diagrams/object_oriented/__init__.py
+++ diagrams/object_oriented/__init__.py
@@ -5,7 +5,7 @@ diagrams.object_oriented
 This module provides an object oriented API for drawing diagrams using
 the tkinter graphical interface.
 """
-from diagrams.object_oriented.color import Color
-from diagrams.object_oriented.coordinates import Coordinates
-from diagrams.object_oriented.diagram import Diagram
-from diagrams.object_oriented.components import Rectangle, Text, Arrow, RectangularNode
+from diagrams.object_oriented.color import *
+from diagrams.object_oriented.coordinates import *
+from diagrams.object_oriented.diagram import *
+from diagrams.object_oriented.components import *
